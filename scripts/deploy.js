e1b12004a2ee3f6024a92aecf8d7d319547586ae
dzekicb <bogdan.dzekic@tenderly.co>
2024-10-16 16:09:28 +0200

    latest addition 1

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,4 +1,5 @@
 const hre = require("hardhat");
+const fs = require('fs');
 
 async function main() {
   const Counter = await hre.ethers.getContractFactory("Counter");
@@ -6,6 +7,13 @@ async function main() {
   const counter = await Counter.deploy();
   await counter.waitForDeployment();
   console.log("Counter deployed to:", await counter.getAddress());
+
+
+const deploymentInfo = {
+    counterAddress: await counter.getAddress()
+};
+fs.writeFileSync('deployment.json', JSON.stringify(deploymentInfo, null, 2));
+console.log('Deployment info saved to deployment.json');
 }
 
 main()
