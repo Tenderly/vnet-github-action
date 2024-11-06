847375a30f814be6da1fe897a35b1f683b79209a
dzekicb <bogdan.dzekic@tenderly.co>
2024-11-06 21:40:16 +0100

    action complete refactor

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,24 +1,18 @@
 require("@nomicfoundation/hardhat-toolbox");
-require("dotenv").config();
 require("@tenderly/hardhat-tenderly");
-
-const TENDERLY_RPC_URL = process.env.TENDERLY_ADMIN_RPC_URL;
-const TENDERLY_ACCESS_KEY = process.env.TENDERLY_ACCESS_KEY;
-const TENDERLY_PROJECT_SLUG = process.env.TENDERLY_PROJECT_SLUG;
-const TENDERLY_ACCOUNT_SLUG = process.env.TENDERLY_ACCOUNT_SLUG;
+require("dotenv").config();
 
 module.exports = {
   solidity: "0.8.19",
   networks: {
     vnet: {
-      url: TENDERLY_RPC_URL || "",
-      chainId: 1,
-    },
+      url: process.env.TENDERLY_ADMIN_RPC_URL,
+      chainId: parseInt(process.env.TENDERLY_CHAIN_ID)
+    }
   },
   tenderly: {
-    project: TENDERLY_PROJECT_SLUG,
-    username: TENDERLY_ACCOUNT_SLUG,
-    accessKey: TENDERLY_ACCESS_KEY,
-    configPath: `${process.env.HOME}/.tenderly/config.yaml`
-  },
+    project: process.env.TENDERLY_PROJECT_SLUG,
+    username: process.env.TENDERLY_ACCOUNT_SLUG,
+    accessKey: process.env.TENDERLY_ACCESS_KEY
+  }
 };
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -2,23 +2,23 @@ const hre = require("hardhat");
 const fs = require('fs');
 
 async function main() {
-  const Counter = await hre.ethers.getContractFactory("Counter");
-  console.log("Deploying Counter...");
-  const counter = await Counter.deploy();
-  await counter.waitForDeployment();
-  console.log("Counter deployed to:", await counter.getAddress());
+    const Counter = await hre.ethers.getContractFactory("Counter");
+    console.log("Deploying Counter...");
+    const counter = await Counter.deploy();
+    await counter.waitForDeployment();
+    console.log("Counter deployed to:", await counter.getAddress());
 
 
-const deploymentInfo = {
-    counterAddress: await counter.getAddress()
-};
-fs.writeFileSync('deployment.json', JSON.stringify(deploymentInfo, null, 2));
-console.log('Deployment info saved to deployment.json');
+    const deploymentInfo = {
+        counterAddress: await counter.getAddress()
+    };
+    fs.writeFileSync('deployment.json', JSON.stringify(deploymentInfo, null, 2));
+    console.log('Deployment info saved to deployment.json');
 }
 
 main()
-  .then(() => process.exit(0))
-  .catch((error) => {
-    console.error(error);
-    process.exit(1);
-  });
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
